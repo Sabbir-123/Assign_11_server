4ad94ad2fe495a443c2404d31a44e8c6a22baee5
Md Sabbir Ahmed <mdsabbirahmed@Mds-MacBook-Air.local>
2022-11-09 22:22:18 +0600

    added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 app.use(cors());
@@ -16,10 +16,37 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
+try{
+    await client.connect();
+const myServiceCollection = client.db('assignment').collection('services');
+app.get('/services', async(req, res)=>{
+    const query = {};
+    const cursor = myServiceCollection.find(query);
+    const services = await cursor.toArray();
+    res.send(services)
+})
+
+app.get('/services/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const service = await myServiceCollection.findOne(query);
+    res.send(service);
+});
+app.get('/servicelimit', async(req, res) =>{
+    const query = {}
+    const cursor = myServiceCollection.find(query);
+    const service = await cursor.limit(3).toArray();
+    res.send( service);
+});
+}
+catch(error){
+    console.log(err.name, err.message)
+}
 
 }
 
-run().catch(err=>console.log(err.name, err.message))
+
+run()
 app.listen(port , ()=>{
     console.log(`Assignment 11 is loading from ${port}`)
 })
